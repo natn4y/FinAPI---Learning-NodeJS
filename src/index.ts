c4f2f5896d2ec5e16d6182761d2d068307d99a51
Natanael Araújo <natanael@hotmail.co.jp>
2022-11-07 20:01:59 -0300

    ✨ Feat: Cadastro de conta

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
 import express from "express";
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+
+interface customersProps extends Array<{}> {
+  cpf: string
+  name: string
+  id: number
+  statement: []
+}
+
+const customers = [] as unknown as customersProps;
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
+})
 
 app.listen(port, () => {
   console.log(`[SERVER RUNNING]: listening on port: ${port}`);
